863a14c624ae20895507227ac59b4d654c73749b
philipk8 <philipk876@gmail.com>
2021-01-21 16:20:40 -0600

    completed splash page css

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -29,18 +29,69 @@ const SplashPage = () => (
       <section className='section-content'>
 
         <section className='left-section'>
+          <p className='section-header'>
+            Less stress when sharing expenses <span>on trips.</span>
+          </p>
+          
+          <div className='section-icons'>
+            <img src="assets/airplane-icon.png" alt=""/>
+            <img src="assets/house-icon.png" alt=""/>
+            <img src="assets/heart-icon.png" alt=""/>
+            <img src="assets/star-icon.png" alt=""/>
+          </div>
+          
+          <p className='section-text'>Keep track of your shared expenses and balances with housemates, trips, groups, friends, and family.   
+          </p>
+          
+          <Link to="/signup" >
+            <button className='section-signup-button'>
+            Sign up
+            </button>
+          </Link>
+
+          <p className='section-footer'>Free for 
+          <Link to="/" className='section-footer-link'>  iPhone</Link>
+          <Link to="/" className='section-footer-link'>, Android, </Link>
+          and web.</p>
 
         </section>
 
         <section className='right-section'>
-
+            <img className='airplane'src="assets/airplane-icon.png" alt=""/>
         </section>
-      
       </section>
-
     </section>
 
+    <footer>
+      <section className="main-footer">
+          <section className="left-footer">
+              <div className='footer-expense-types'>
+                  <span>Toolkit</span>
+                  <p>Javascript</p>
+                  <p>React/Redux</p>
+                  <p>Ruby on Rails</p>
+              </div>
+              <div className='footer-account'>
+                  <span>Account</span>
+                  <br></br>
+                  <Link to="/login" className='footer-link'>Log in</Link>
+                  <br></br>
+                  <Link to="/signup" className='footer-link'>Sign Up</Link>
+              </div>
+              <div className='footer-more'>
+                  <span>More</span>
+                  <p></p>
+                  <Link to="/" className='footer-link'>LinkedIn</Link>
+                  <p></p>
+                  <Link to="/" className='footer-link'>Github</Link>
+              </div>
 
+          </section>
+          <section className="right-footer">
+              <span>Made with :) in Providence, RI, USA</span>
+          </section>
+      </section>
+    </footer>
 
   </div>
 );
