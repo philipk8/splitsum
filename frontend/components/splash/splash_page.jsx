49deb9fcb559622ba41a00058db4a2b4b15fb655
philipk8 <philipk876@gmail.com>
2021-01-21 13:21:59 -0600

    working on middle section of splash page

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -2,26 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SplashPage = () => (
-  <div>
+  <div className='main-page'>
+   
    <header className='splash-nav'>
-    
+   
     <nav className='left-nav'>
       <img className='logo' src="assets/splitsum_logo.png" alt=""/>
-      <Link to="/" className='word-logo'>Splitwise</Link>
+      <Link to="/" className='word-logo'>Splitsum</Link>
     </nav>
     
     <nav className='right-nav'>
-      <Link to="/login">Log in</Link>
-      
-      <Link to="/signup" className='signup-button'>
-        <button>
+      <Link to="/login" className='login-link'>Log in</Link>
+      <Link to="/signup" >
+        <button className='signup-button'>
           Sign up
         </button>
       </Link>
     </nav>
 
-
     </header>
+
+    {/* ************** */}
+
+    <section className='main-section'>
+
+      <section className='section-content'>
+
+        <section className='left-section'>
+
+        </section>
+
+        <section className='right-section'>
+
+        </section>
+      
+      </section>
+
+    </section>
+
+
+
   </div>
 );
 
