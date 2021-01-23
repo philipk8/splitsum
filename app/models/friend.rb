7f36a65af5462d3bee47d4c6daf1d787db13fae2
philipk8 <philipk876@gmail.com>
2021-01-23 13:59:24 -0600

    fixed spelling of presence

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,6 @@
 class Friend < ApplicationRecord
   validates :user_id, uniqueness: { scope: :friend_id}
-  validates :user_id, :friend_id, prescence: true
+  validates :user_id, :friend_id, presence: true
 
   # belongs_to :user,
   #   foreign_key: :friend_id,
